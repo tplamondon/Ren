b383cd1c6076035c0ab98f3e3cfe039b23c66d07
tplamondon <tplamondon@tutanota.de>
2021-05-19 19:32:51 -0700

    finished greetCommand hopefully

--- cogs/greet/greet.py
+++ cogs/greet/greet.py
@@ -37,8 +37,16 @@ class Greet(commands.Cog):
         -----------
 
         """
+        USERPING = "{USER}"
+
         await ctx.trigger_typing()
-        message =  await self.getRandomMessage()
+        rawMessage =  await self.getRandomMessage()
+        splitMessage = rawMessage.split("{USER}")
+        message = ""
+        for x in range(len(splitMessage)):
+            message+=splitMessage[x]
+            if(x<len(splitMessage)):
+                message+=discord.Member
         await ctx.send(message)
 
     @greetCommand.command(name="add")
@@ -49,5 +57,3 @@ class Greet(commands.Cog):
         -----------
         greeting: a greeting that may or may not include {USER} in the message
         """
-
-        USERPING = "{USER}"
