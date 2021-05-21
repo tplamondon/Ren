3bab0a1495b56f7afccbe821ebbbd8df5f77dd9c
tplamondon <tplamondon@tutanota.de>
2021-05-20 22:00:04 -0700

    fixed issue with random selection from dictionary

--- cogs/welcome/welcome.py
+++ cogs/welcome/welcome.py
@@ -58,7 +58,7 @@ class Welcome(commands.Cog):  # pylint: disable=too-many-instance-attributes
         if not greetings:
             return "Welcome to the server {USER}"
         else:
-            return random.choice(greetings)[1]
+            return random.choice(list(greetings.values()))
 
     # The async function that is triggered on new member join.
     @commands.Cog.listener()
